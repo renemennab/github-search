0f456aa7ba0d2fdce11cc5368859899fcbc7c5b0
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2020-01-31 20:46:38 -0300

    create home page layout

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
-
-export function Home(props:any) {
-    return <div>
-        home
-    </div>
+import styled from 'styled-components'
+import Title from './Title'
+import Search from './Search'
+export function Home(props: any) {
+    return (
+        <HomeStyles>
+            <Title />
+            <Search />
+        </HomeStyles>
+    )
 }
+const HomeStyles = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    #title {
+        font-size: 60px;
+        margin-bottom: 10px;
+    }
+
+    @media screen and (max-width: 600px) {
+        #title {
+        font-size: 40px;
+    }
+`
