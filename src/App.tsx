fbd45eb4c6adbb0f5b2ce9858b25b4a182a32b19
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2020-02-01 13:12:28 -0300

    re structure app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
-import { Home } from './Home'
+import Search from './Search'
 import { Result } from './Result'
-import { NotFound } from './NotFound'
 
 const App: React.FC = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/result" exact component={Result} />
-                <Route path="/notFound" exact component={NotFound} />
-                <Route path="/" render={() => <div>404</div>} />
+                <Route path="/" render={(props: any) => <Search />} />
+                <Route path="/result" exact render={(props: any) => <Result />} />
             </Switch>
         </BrowserRouter>
     )
