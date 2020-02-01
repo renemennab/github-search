4bf8c35c55f87665f00a7e648a1d7e048744cf58
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2020-02-01 12:37:15 -0300

    add lint and fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import './App.css';
-import { Home } from "./Home";
-import { Result } from "./Result";
-import { NotFound } from "./NotFound";
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './App.css'
+import { Home } from './Home'
+import { Result } from './Result'
+import { NotFound } from './NotFound'
 
 const App: React.FC = () => {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/result" exact component={Result} />
-          <Route path="/notFounds" exact component={NotFound} />
-          <Route path="/" render={() => <div>404</div>} />
-        </Switch>
-      </BrowserRouter>
-    );
-  };
-  
-  export default App;
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/result" exact component={Result} />
+                <Route path="/notFound" exact component={NotFound} />
+                <Route path="/" render={() => <div>404</div>} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default App
