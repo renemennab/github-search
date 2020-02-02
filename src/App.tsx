b2a514aa61c98f6096ed794862b8017d0913b9af
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2020-02-02 11:27:02 -0300

    optimize promisse func

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,28 @@ import Search from './Search'
 import { Result } from './Result'
 
 const App: React.FC = () => {
-    const [RequestProfileResponse, setRequestProfileResponse] = useState({})
-    const [RequestReposResponse, setRequestReposResponse] = useState({})
+    const [RequestResponse, setRequestResponse] = useState({ profile: {}, repo: {} })
 
-    console.log(RequestProfileResponse)
-    console.log(RequestReposResponse)
+    console.log(RequestResponse)
     function handleSubmitForm(event: any, SearchString: string) {
         event.preventDefault()
         try {
             Promise.all([
-                fetch(`https://api.github.com/users/${SearchString}`),
-                fetch(`https://api.github.com/users/${SearchString}/repos`)
-            ]).then(([profile, repo]) => {
-                setRequestProfileResponse(profile.json())
-                setRequestReposResponse(repo.json())
-            })
+                fetch(`https://api.github.com/users/${SearchString}`).then(function(response) {
+                    return response.json()
+                }),
+                fetch(`https://api.github.com/users/${SearchString}/repos`).then(function(response) {
+                    return response.json()
+                })
+            ])
+                .then(function(responses) {
+                    return responses.map(function(response) {
+                        return response
+                    })
+                })
+                .then(function(data) {
+                    setRequestResponse({ profile: data[0], repo: data[1] })
+                })
         } catch (err) {
             console.error(err)
         }
@@ -29,7 +36,11 @@ const App: React.FC = () => {
         <BrowserRouter>
             <Switch>
                 <Route path="/" render={(props: any) => <Search {...props} submitHandler={handleSubmitForm} />} />
-                <Route path="/result" exact render={(props: any) => <Result />} />
+                <Route
+                    path="/result"
+                    exact
+                    render={(props: any) => <Result profile={RequestResponse.profile} repo={RequestResponse.repo} />}
+                />
             </Switch>
         </BrowserRouter>
     )
