8afe667def8e1a843028970b01c3f412d8f0866c
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2020-02-02 13:57:57 -0300

    configure not found component

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+import { colors } from './constants'
 
-export function NotFound(props:any) {
-    return <div>
-        notfound
-    </div>
+export function NotFound(props: any) {
+    return <NotFoundStyles>User not found :&#40;</NotFoundStyles>
 }
+
+const NotFoundStyles = styled.div`
+    width: 100%;
+    font-size: 40px;
+    text-align: center;
+    margin-top: 125px;
+    color: ${colors.purple};
+`
