a4e01d781ce5973a03cc30bb860e7c39fc41d509
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2020-07-01 13:48:05 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-search/precache-manifest.effc8b84b7968e5c7045858c9308052d.js"
+  "/github-search/precache-manifest.f53802be5871602f37a97b6bbe7d5b08.js"
 );
 
 self.addEventListener('message', (event) => {
