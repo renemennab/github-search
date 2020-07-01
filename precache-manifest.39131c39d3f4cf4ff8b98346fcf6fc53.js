6365fc1d19d62f277b968cefa0012d99ca8f528e
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2020-07-01 12:34:11 -0300

    Updates

---
+++ precache-manifest.39131c39d3f4cf4ff8b98346fcf6fc53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138daf3ed5bbf47d56d8ea64b3ef41c3",
+    "url": "/github-api/index.html"
+  },
+  {
+    "revision": "4dda64996fd6c7bbc49b",
+    "url": "/github-api/static/css/main.db0ef0dc.chunk.css"
+  },
+  {
+    "revision": "bca49007a7f6ef59f827",
+    "url": "/github-api/static/js/2.09874b53.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/github-api/static/js/2.09874b53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dda64996fd6c7bbc49b",
+    "url": "/github-api/static/js/main.641c8a2b.chunk.js"
+  },
+  {
+    "revision": "b0de9ca7736a045d283f",
+    "url": "/github-api/static/js/runtime-main.383b0c47.js"
+  }
+]);
--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const colors = {
-    purple: '#ac53f2',
-    grey: '#5c5c5c',
-    shadow: '0 0 4px 0px rgba(0,0,0,0.4)'
-}
