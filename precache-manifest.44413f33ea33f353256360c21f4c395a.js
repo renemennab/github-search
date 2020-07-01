3d565844bbeca5b69f0f16fbacc32879c057c00a
Renê-Boaventura-Neto <reneboaventuraneto@gmail.com>
2020-07-01 13:42:11 -0300

    Updates

---
+++ precache-manifest.44413f33ea33f353256360c21f4c395a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed73e59279a0a92faaf1774a34f6117",
+    "url": "/github-api/index.html"
+  },
+  {
+    "revision": "8d8ae4c7668aba0d42e8",
+    "url": "/github-api/static/css/main.db0ef0dc.chunk.css"
+  },
+  {
+    "revision": "8881d497c820e8571408",
+    "url": "/github-api/static/js/2.fcd2b89a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-api/static/js/2.fcd2b89a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8ae4c7668aba0d42e8",
+    "url": "/github-api/static/js/main.1176c42f.chunk.js"
+  },
+  {
+    "revision": "b62a5b320616deedee83",
+    "url": "/github-api/static/js/runtime-main.ce36abcc.js"
+  }
+]);
